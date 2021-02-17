e88656cf689b605b845a075ba1c4b59a4b8a002c
Vachilavit Prachyapattanapong <vachilavit.p@gmail.com>
2021-02-18 01:08:54 +0700

    Update unit test

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1
+	github.com/stretchr/testify v1.5.1
 	google.golang.org/grpc v1.35.0
 	google.golang.org/protobuf v1.25.0
 )
--- internal/auth/pkg/token_test.go
+++ internal/auth/pkg/token_test.go
@@ -5,52 +5,37 @@ import (
 	"time"
 
 	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/vachilavit/auth-system/internal/auth/testdata"
-	testhelper "github.com/vachilavit/auth-system/pkg"
 )
 
 func TestWithAccessTokenExpAt(t *testing.T) {
 	arg := time.Now().Add(time.Minute)
 	want := jwt.At(arg)
-
 	tk := New(WithAccessTokenExpAt(arg))
-
-	if !tk.AccessTokenClaims.ExpiresAt.Time.Equal(want.Time) {
-		t.Errorf("WithAccessTokenExpAt(%v) got = %v, want = %v", arg, tk.AccessTokenClaims.ExpiresAt, want)
-	}
+	assert.Equal(t, want.Time, tk.AccessTokenClaims.ExpiresAt.Time, "AccessTokenClaims.ExpiresAt.Time should be equal")
 }
 
 func TestWithRefreshTokenExpAt(t *testing.T) {
 	arg := time.Now().Add(time.Hour)
 	want := jwt.At(arg)
-
 	tk := New(WithRefreshTokenExpAt(arg))
-
-	if !tk.RefreshTokenClaims.ExpiresAt.Time.Equal(want.Time) {
-		t.Errorf("WithRefreshTokenExpAt(%v) got = %v, want = %v", arg, tk.AccessTokenClaims.ExpiresAt, want)
-	}
+	assert.Equal(t, want.Time, tk.RefreshTokenClaims.ExpiresAt.Time, "RefreshTokenClaims.ExpiresAt.Time should be equal")
 }
 
 func TestWithAccessTokenSecretKey(t *testing.T) {
 	arg := "secretkey"
 	want := arg
-
 	tk := New(WithAccessTokenSecretKey(arg))
-
-	if tk.AccessTokenSecretKey != want {
-		t.Errorf("WithAccessTokenSecretKey(%v) got = %v, want = %v", arg, tk.AccessTokenSecretKey, want)
-	}
+	assert.Equal(t, want, tk.AccessTokenSecretKey, "AccessTokenSecretKey should be equal")
 }
 
 func TestWithRefreshTokenSecretKey(t *testing.T) {
 	arg := "secretkey2"
 	want := arg
-
 	tk := New(WithRefreshTokenSecretKey(arg))
-
-	if tk.RefreshTokenSecretKey != want {
-		t.Errorf("WithRefreshTokenSecretKey(%v) got = %v, want = %v", arg, tk.RefreshTokenSecretKey, want)
-	}
+	assert.Equal(t, want, tk.RefreshTokenSecretKey, "RefreshTokenSecretKey should be equal")
 }
 
 func TestComposeClaimsWithUser(t *testing.T) {
@@ -58,17 +43,10 @@ func TestComposeClaimsWithUser(t *testing.T) {
 	wantUsername := arg.Username
 	wantUserID := arg.ID
 	wantSaltForRefreshTokenSecretKey := arg.HashedPassword
-
 	tk := New(ComposeClaimsWithUser(arg))
-
-	switch {
-	case tk.AccessTokenClaims.Username != wantUsername:
-		t.Errorf("ComposeClaimsWithUser(%v) got = %v, want = %v", arg, tk.AccessTokenClaims.Username, wantUsername)
-	case tk.RefreshTokenClaims.UserID != wantUserID:
-		t.Errorf("ComposeClaimsWithUser(%v) got = %v, want = %v", arg, tk.RefreshTokenClaims.ID, wantUserID)
-	case tk.SaltForRefreshTokenSecretKey != wantSaltForRefreshTokenSecretKey:
-		t.Errorf("ComposeClaimsWithUser(%v) got = %v, want = %v", arg, tk.RefreshTokenSecretKey, wantSaltForRefreshTokenSecretKey)
-	}
+	assert.Equal(t, wantUsername, tk.AccessTokenClaims.Username, "AccessTokenClaims.Username should be equal")
+	assert.Equal(t, wantUserID, tk.RefreshTokenClaims.UserID, "RefreshTokenClaims.UserID should be equal")
+	assert.Equal(t, wantSaltForRefreshTokenSecretKey, tk.SaltForRefreshTokenSecretKey, "SaltForRefreshTokenSecretKey should be equal")
 }
 
 func TestTokenGenerate(t *testing.T) {
@@ -76,20 +54,13 @@ func TestTokenGenerate(t *testing.T) {
 	tk := New(ComposeClaimsWithUser(user), WithAccessTokenExpAt(time.Unix(0, 0)), WithRefreshTokenExpAt(time.Unix(0, 0)))
 
 	// {"username": "admin", "exp": 0}
-	wantAccessTokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjowfQ.5LM5CD5guK4UZe4KwKYjeih3Yh1m_DPlTTzfF8D9r0"
+	wantAccessTokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjowfQ.5LM5CD5guK4UZe4KwKYjeih3Yh1m_DPlTTzfF8D9r0M"
 	// {"userID": "e3e832db-b725-4fd5-94be-0ca4bc112e7d", "exp": 0}
 	wantRefreshTokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiJlM2U4MzJkYi1iNzI1LTRmZDUtOTRiZS0wY2E0YmMxMTJlN2QiLCJleHAiOjB9.RcKHvdZnfAKQlvR2PZ8_BcgU1Fn7pi2divr-Jl2wAdk"
 
 	accessTokenString, refreshTokenString, err := tk.Generate()
-	if err != nil {
-		t.Errorf("TokenGenerate() error = %v gotAccessTokenString = %v, gotAccessRefreshTokenString = %v, wantAccessTokenString = %v, wantRefreshTokenString = %v", err, accessTokenString, refreshTokenString, wantAccessTokenString, wantRefreshTokenString)
-	}
+	require.Nil(t, err, "Generate() should be nil")
 
-	switch {
-	case accessTokenString != wantAccessTokenString:
-		testhelper.Assert(t, false, "dsadfe")
-		// t.Errorf("TokenGenerate() gotAccessTokenString = %v, wantAccessTokenString = %v", accessTokenString, wantAccessTokenString)
-	case refreshTokenString != wantRefreshTokenString:
-		t.Errorf("TokenGenerate() gotAccessRefreshTokenString = %v, wantRefreshTokenString = %v", refreshTokenString, wantRefreshTokenString)
-	}
+	assert.Equal(t, wantAccessTokenString, accessTokenString, "accessTokenString should be equal")
+	assert.Equal(t, wantRefreshTokenString, refreshTokenString, "refreshTokenString should be equal")
 }
